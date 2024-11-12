9bdbd56c3ee0afde4be0638f201932db34053d78
许霖 <l-xu22@mails.tsinghua.edu.cn>
2024-11-12 21:21:00 +0800

    Complete features regard to collections

--- main.go
+++ main.go
@@ -1,37 +1,23 @@
 package main
 
 import (
-	"Serenesongserver/routers"
-	"os"
-	"log"
-	"context"
+	"Serenesongserver/config"
+	"Serenesongserver/controllers"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	// 从 miniapp.env 文件中加载环境变量
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	appid := os.Getenv("APPID")
-	secret := os.Getenv("SECRET")
-
-	// 初始化mongodb
-	mongodburi := os.Getenv("MONGODBURI")
-	clientOptions := options.Client().ApplyURI(mongodburi)
-	client, err := mongo.Connect(context.Background(), clientOptions)
-	if err != nil {
-		log.Fatal(err)
-	}
+	config.LoadEnv()
 
 	router := gin.Default()
-	router.GET("/login", func(c *gin.Context) {
-		routers.LoginHandler(c, appid, secret, client)
-	})
+
+	router.GET("/login", controllers.Login)
+
+	router.POST("/createCollection", controllers.CreateCollection)
+	router.POST("/deleteCollection", controllers.DeleteCollection)
+	router.POST("/addToCollection", controllers.AddToCollection)
+	router.POST("/removeFromCollection", controllers.RemoveFromCollection)
+
 	router.Run(":8080")
 }
