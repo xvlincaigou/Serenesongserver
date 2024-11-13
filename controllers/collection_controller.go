b2fca116f2c36a47d16d3127ab9c99d83addfdb1
许霖 <l-xu22@mails.tsinghua.edu.cn>
2024-11-13 09:46:14 +0800

    Add collection and comment apis.

--- controllers/collection_controller.go
+++ controllers/collection_controller.go
@@ -47,3 +47,33 @@ func RemoveFromCollection(c *gin.Context) {
 	}
 	services.RemoveFromCollectionHandler(c, collectionID, ciID, token)
 }
+
+func GetAllColletions(c *gin.Context) {
+	token := c.Query("token")
+	if token == "" {
+		c.JSON(400, gin.H{"error": "Token is required"})
+		return
+	}
+	services.GetAllCollectionsHandler(c, token)
+}
+
+func GetAllColletionItems(c *gin.Context) {
+	collectionID := c.Query("collectionID")
+	token := c.Query("token")
+	if collectionID == "" || token == "" {
+		c.JSON(400, gin.H{"error": "CollectionID and token are required"})
+		return
+	}
+	services.GetAllCollectionItemsHandler(c, collectionID, token)
+}
+
+func ModifyCollectionComment(c *gin.Context) {
+	ciID := c.Query("ciID")
+	comment := c.Query("comment")
+	token := c.Query("token")
+	if ciID == "" || comment == "" || token == "" {
+		c.JSON(400, gin.H{"error": "CollectionID, Comment and token are required"})
+		return
+	}
+	services.ModifyCollectionCommentHandler(c, ciID, comment, token)
+}
--- main.go
+++ main.go
@@ -12,12 +12,15 @@ func main() {
 
 	router := gin.Default()
 
-	router.GET("/login", controllers.Login)
+	router.POST("/login", controllers.Login)
 
+	router.GET("/getAllCollections", controllers.GetAllColletions)
+	router.GET("/getAllColletionItems", controllers.GetAllColletionItems)
 	router.POST("/createCollection", controllers.CreateCollection)
 	router.POST("/deleteCollection", controllers.DeleteCollection)
 	router.POST("/addToCollection", controllers.AddToCollection)
 	router.POST("/removeFromCollection", controllers.RemoveFromCollection)
+	router.POST("/modifyCollectionComment", controllers.ModifyCollectionComment)
 
 	router.Run(":8080")
 }
