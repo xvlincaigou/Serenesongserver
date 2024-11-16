9b3d25ef94a4b513381b3e6cb561f499c054b615
bbbpimasheep <tadatatsuno@gmail.com>
2024-11-16 14:36:11 +0800

    Schema modified

--- models/author.go
+++ models/author.go
@@ -1,10 +1,11 @@
 package models
 
 import (
-
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Author struct {
-	Name	string `bson:"name" json:"name"`
-	Bio 	string `bson:"bio" json:"bio"`
+	ID    	primitive.ObjectID	`bson:"_id,omitempty" json:"_id,omitempty"`
+	Name	string 				`bson:"name" json:"name"`
+	Bio 	string 				`bson:"bio" json:"bio"`
 }
--- models/modern_work.go
+++ models/modern_work.go
@@ -1,19 +1,21 @@
 package models
 
 import (
+	"time"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type Ci struct {
+type ModernWork struct {
 	ID    		primitive.ObjectID	`bson:"_id,omitempty" json:"_id,omitempty"`
-	Author 		string 				`bson:"author" json:"author"`
+	Author 		primitive.ObjectID 	`bson:"author" json:"author"`
 	Title 		string 				`bson:"title, omitempty" json:"title,omitempty"`
-	Age         string 				`bson:"age, omitempty" json:"age,omitempty"`
+	// Age         string 				`bson:"age, omitempty" json:"age,omitempty"`
 	Content 	[]string 			`bson:"content" json:"content"`
 	Cipai   	[]string 			`bson:"cipai" json:"cipai"`
 	Xiaoxu 		string 				`bson:"prologue,omitempty" json:"prologue,omitempty"`
 	// IsModern 	bool 				`bson:"is_modern" json:"is_modern"`
-	// Public 		bool 				`bson:"public" json:"public"`
+	IsPublic 	bool 				`bson:"is_public" json:"public"`
 	Tags 		[]string 			`bson:"tags,omitempty" json:"tags,omitempty"`
-	// CreatedAt 	time.Time           `bson:"created_at,omitempty" json:"created_at,omitempty"`
+	CreatedAt 	time.Time           `bson:"created_at,omitempty" json:"created_at,omitempty"`
+	UpdatedAt 	time.Time           `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
 }
