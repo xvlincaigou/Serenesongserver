0c1dc6b3f08f93e8cac2715f7fe06b282f76c18f
bbbpimasheep <tadatatsuno@gmail.com>
2024-12-11 01:46:10 +0800

    Add functions: processing avater & username & dynamics

--- models/dynamic.go
+++ models/dynamic.go
@@ -12,5 +12,18 @@ type Dynamic struct {
 	// 在type为2的时候，这个字段是对应的收藏夹item的id。
 	CollectionItemId primitive.ObjectID `bson:"collection_item_id"`
 	// 这个动态下面的评论
-	Comments primitive.ObjectID `bson:"comments"`
+	Comments []primitive.ObjectID `bson:"comments"`
+}
+
+type DynamicContent struct {
+	// type 0: 单纯转发一首古代的词。1：单纯转发一首现代的词（其实这个type也可以用来“公开发布自己的诗词”）。2：转发自己的收藏夹批注。
+	Type 	int 			`bson:"type"`
+	// 在type为0的时候，这个字段是对应的古代词的id。
+	Classic Ci 				`bson:"classic"`
+	// 在type为1的时候，这个字段是对应的现在作品的id。
+	Modern 	ModernWork 		`bson:"modern"`
+	// 在type为2的时候，这个字段是对应的收藏夹item的id。
+	Item 	CollectionItem 	`bson:"collection_item"`
+	// 这个动态下面的评论
+	Comments 	[]Comment 	`bson:"comments"`
 }
